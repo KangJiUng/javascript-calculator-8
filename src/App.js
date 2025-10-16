01dbd4009e7169d232cf7a644a742083f92065ab
KangJiUng <woong236406@gmail.com>
2025-10-16 18:10:02 +0900

    feat: 기본 구분자 파싱 및 숫자 변환 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Console } from "@woowacourse/mission-utils";
 
+function parseInput(input) {
+  // 기본 구분자 쉼표(,)와 콜론(:)으로 파싱 및 숫자로 변환
+  const numbers = input.split(/[, :]/).map(Number);
+
+  return numbers;
+}
+
 class App {
   async run() {
     const input = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
+
+    const numbers = parseInput(input);
   }
 }
 
